f53ada282bb22394d93d044ed603c80f772a9f09
Documenter.jl <documenter@juliadocs.github.io>
2021-07-14 14:02:49 +0000

    build based on b072699

---
+++ previews/PR10/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ITensorsGrad","category":"page"},{"location":"#ITensorsGrad","page":"Home","title":"ITensorsGrad","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ITensorsGrad.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ITensorsGrad]","category":"page"}]
+}
